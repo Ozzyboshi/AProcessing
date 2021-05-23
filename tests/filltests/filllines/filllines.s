20dc7f6c10a260b8be939a162a75324eab420236
Ozzyboshi <gun101@email.it>
2021-05-23 22:22:59 +0000

    new vertical fills test

--- tests/filltests/filllines/filllines.s
+++ tests/filltests/filllines/filllines.s
@@ -6,6 +6,7 @@
 	XDEF _filline_test5
 	XDEF _filline_test6
 	XDEF _filline_test7
+	XDEF _filline_test8
 	
 	SECTION PROCESSING,CODE_F
 
@@ -16,6 +17,8 @@
 	include "../../../libs/rasterizers/globaloptions.s"
 
 _filline_test1:
+
+	RESETFILLTABLE
 
 	lea LINEVERTEX_START_FINAL,a1
 	move.w #1,(a1)+
@@ -37,6 +40,8 @@ _filline_test1:
 
 _filline_test2:
 
+	RESETFILLTABLE
+
 	lea LINEVERTEX_START_FINAL,a1
 	move.w #8,(a1)+
 	move.w #5,(a1)+
@@ -56,6 +61,8 @@ _filline_test2:
 	rts
 
 _filline_test3:
+
+	RESETFILLTABLE
 
 	lea LINEVERTEX_START_FINAL,a1
 	move.w #10,(a1)+
@@ -77,6 +84,8 @@ _filline_test3:
 
 _filline_test4:
 
+	RESETFILLTABLE
+
 	lea LINEVERTEX_START_FINAL,a1
 	move.w #1,(a1)+
 	move.w #1,(a1)+
@@ -97,6 +106,8 @@ _filline_test4:
 
 _filline_test5:
 
+	RESETFILLTABLE
+
 	lea LINEVERTEX_START_FINAL,a1
 	move.w #8,(a1)+
 	move.w #5,(a1)+
@@ -116,6 +127,8 @@ _filline_test5:
 	rts
 
 _filline_test6:
+
+	RESETFILLTABLE
 	
 	lea LINEVERTEX_START_FINAL,a1
 	move.w #1,(a1)+
@@ -142,12 +155,26 @@ _filline_test6:
 	rts
 
 _filline_test7:
+
 	RESETFILLTABLE
+
 	lea LINEVERTEX_START_FINAL,a1
 	move.w #1,(a1)+
 	move.w #1,(a1)+
 	move.w #1,(a1)+
 	move.w #2,(a1)+
+	bsr.w ammxlinefill
+
+	bsr.w processing_fill_table_addr ; fill table addr in d0
+	rts
+
+_filline_test8:
+	RESETFILLTABLE
+	lea LINEVERTEX_START_FINAL,a1
+	move.w #0,(a1)+
+	move.w #0,(a1)+
+	move.w #1,(a1)+
+	move.w #7,(a1)+
 	bsr.w ammxlinefill
 
 	bsr.w processing_fill_table_addr ; fill table addr in d0
