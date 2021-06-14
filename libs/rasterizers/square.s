61f95adb737760c094bf997ea803bd839d191320
Ozzyboshi <gun101@email.it>
2021-06-14 20:24:04 +0000

    rotated square test draw add

--- libs/rasterizers/square.s
+++ libs/rasterizers/square.s
@@ -1,8 +1,10 @@
 SQUARE:
-    movem.l d0-d6/a0-a6,-(sp)
+    movem.l d0-d7/a0-a1,-(sp)
 
     RESETFILLTABLE
     lea LINEVERTEX_START_FINAL,a1
+    
+    ; Start of line 1
     move.w d0,d6
     move.w d1,d7
 
@@ -16,44 +18,65 @@ SQUARE:
     move.w d6,d0
     move.w d7,d1
     ; add width
-    add.w d2,d0
+    add.w d5,d0
+    bsr.w point_execute_transformation
+    ; save transformed values
+    move.w d0,(a1)+
+    move.w d1,(a1)+
+    bsr.w ammxlinefill
+    ; End of Line 1
+
+
+    ; Start of line 2
+    lea LINEVERTEX_START_FINAL,a1
+    addq #4,a1
+
+    ; restore first point
+    move.w d6,d0
+    move.w d7,d1
+    ; add height
+    add.w d5,d1
+
+    bsr.w point_execute_transformation
+    ; save transformed values
+    move.w d0,(a1)
+    move.w d1,2(a1)
+    bsr.w ammxlinefill
+    ; End of line 2
+
+    ; Start of line 3
+    lea LINEVERTEX_START_FINAL,a1
+    ; restore first point
+    move.w d6,d0
+    move.w d7,d1
+    ; add height and width
+    add.w d5,d0
+    add.w d5,d1
+    bsr.w point_execute_transformation
+    ; save transformed values
+    move.w d0,(a1)+
+    move.w d1,(a1)+
+    bsr.w ammxlinefill
+    ; ENd of line 3
+
+    lea LINEVERTEX_START_FINAL,a1
+    addq #4,a1
+    ; restore first point
+    move.w d6,d0
+    move.w d7,d1
+    ; add width
+    add.w d5,d0
     bsr.w point_execute_transformation
     ; save transformed values
     move.w d0,(a1)+
     move.w d1,(a1)+
     bsr.w ammxlinefill
 
-    ; restore first point
-    move.w d6,d0
-    move.w d7,d1
-    ; add height
-    add.w d2,d1
 
-    bsr.w point_execute_transformation
-    ; save transformed values
-    move.w d0,(a1)+
-    move.w d1,(a1)+
-
-    ; restore first point
-    move.w d6,d0
-    move.w d7,d1
-    ; add height and width
-    add.w d2,d0
-    add.w d2,d1
-    bsr.w point_execute_transformation
-    ; save transformed values
-    move.w d0,(a1)+
-    move.w d1,(a1)+
-
-
-
-    move.w #100,AMMXFILLTABLE_CURRENT_ROW
-	move.w #200,AMMXFILLTABLE_END_ROW
+    move.w #120,AMMXFILLTABLE_CURRENT_ROW
+	move.w #135,AMMXFILLTABLE_END_ROW
 
     bsr.w ammx_fill_table
 
-
-
-    movem.l d0-d6/a0-a6,-(sp)
-
+    movem.l (sp)+,d0-d7/a0-a1
     rts
--- tests/drawingtests/filledpolygons/drawingtestsfilledpolygon.s
+++ tests/drawingtests/filledpolygons/drawingtestsfilledpolygon.s
@@ -3,26 +3,37 @@
 
 	SECTION PROCESSING,CODE_F
 
+	include "../../../libs/rasterizers/globaloptions.s"
 	include "../../../libs/ammxmacros.i"
 	include "../../../libs/matrix/matrix.s"
 	include "../../../libs/trigtables.i"
-	include "../../../libs/rasterizers/point.s"
 	include "../../../libs/rasterizers/processing_bitplanes_fast.s"
 	include "../../../libs/rasterizers/processing_table_plotrefs.s"
 	include "../../../libs/rasterizers/processingclearfunctions.s"
+	include "../../../libs/rasterizers/square.s"
+	include "../../../libs/rasterizers/point.s"
+	include "../../../libs/rasterizers/processingfill.s"
 
 _drawing_test1:
 	
 	CLEARFASTBITPLANES ; Clear fast bitplanes
 	RESET_CURRENT_TRANSFORMATION_MATRIX_Q_10_6
 
+
 	move.w #160,d0
 	move.w #128,d1
 	bsr.w TRANSLATE
 
+
 	ROTATE #45
 
-	SQUARE #-5,#-5,#10
+
+
+	move.w #-5,d0
+	move.w #-5,d1
+	move.w #10,d5
+
+	bsr.w SQUARE ;#-5,#-5,#10
 
 	bsr.w processing_bitplanes_fast_screen0 ; returns bitplanes addr in d0
 	
