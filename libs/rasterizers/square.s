83790a0f4304d3d2598bf774f6d2fbec8b454ade
Ozzyboshi <gun101@email.it>
2021-07-29 17:22:15 +0000

    opt triangle

--- libs/rasterizers/square.s
+++ libs/rasterizers/square.s
@@ -1,80 +1,80 @@
 SQUARE:
-    movem.l d0-d7/a1,-(sp)
+  movem.l    d0-d7/a1,-(sp)
 
-    lea LINEVERTEX_START_FINAL,a1
+  lea        LINEVERTEX_START_FINAL,a1
     
     ; Start of line 1
-    move.w d0,d6
-    move.w d1,d7
+  move.w     d0,d6
+  move.w     d1,d7
 
-	bsr.w point_execute_transformation
+  bsr.w      point_execute_transformation
 
     ; save transformed values
-    move.w d0,(a1)+
-    move.w d1,(a1)+
+  move.w     d0,(a1)+
+  move.w     d1,(a1)+
 
     ; restore first point
-    move.w d6,d0
-    move.w d7,d1
+  move.w     d6,d0
+  move.w     d7,d1
     ; add width
-    add.w d5,d0
-    bsr.w point_execute_transformation
+  add.w      d5,d0
+  bsr.w      point_execute_transformation
     ; save transformed values
-    move.w d0,(a1)+
-    move.w d1,(a1)+
+  move.w     d0,(a1)+
+  move.w     d1,(a1)+
 
-    bsr.w ammxlinefill
+  bsr.w      ammxlinefill
     ; End of Line 1
 
 
     ; Start of line 2
-    lea LINEVERTEX_START_FINAL,a1
-    addq #4,a1
+  lea        LINEVERTEX_START_FINAL,a1
+  addq       #4,a1
 
     ; restore first point
-    move.w d6,d0
-    move.w d7,d1
+  move.w     d6,d0
+  move.w     d7,d1
     ; add height
-    add.w d5,d1
+  add.w      d5,d1
 
-    bsr.w point_execute_transformation
+  bsr.w      point_execute_transformation
     ; save transformed values
-    move.w d0,(a1)
-    move.w d1,2(a1)
-    bsr.w ammxlinefill
+  move.w     d0,(a1)
+  move.w     d1,2(a1)
+  bsr.w      ammxlinefill
     ; End of line 2
 
     ; Start of line 3
-    lea LINEVERTEX_START_FINAL,a1
+  lea        LINEVERTEX_START_FINAL,a1
     ; restore first point
-    move.w d6,d0
-    move.w d7,d1
+  move.w     d6,d0
+  move.w     d7,d1
     ; add height and width
-    add.w d5,d0
-    add.w d5,d1
-    bsr.w point_execute_transformation
+  add.w      d5,d0
+  add.w      d5,d1
+  bsr.w      point_execute_transformation
     ; save transformed values
-    move.w d0,(a1)+
-    move.w d1,(a1)+
-    bsr.w ammxlinefill
+  move.w     d0,(a1)+
+  move.w     d1,(a1)+
+  bsr.w      ammxlinefill
     ; ENd of line 3
 
-    lea LINEVERTEX_START_FINAL,a1
-    addq #4,a1
+  lea        LINEVERTEX_START_FINAL,a1
+  addq       #4,a1
     ; restore first point
-    move.w d6,d0
-    move.w d7,d1
+  move.w     d6,d0
+  move.w     d7,d1
     ; add width
-    add.w d5,d0
-    bsr.w point_execute_transformation
+  add.w      d5,d0
+  bsr.w      point_execute_transformation
     ; save transformed values
-    move.w d0,(a1)+
-    move.w d1,(a1)+
-    bsr.w ammxlinefill
+  move.w     d0,(a1)+
+  move.w     d1,(a1)+
+  bsr.w      ammxlinefill
 
-    move.l AMMX_FILL_FUNCT_ADDR,a1
-    jsr (a1)
+  move.l     AMMX_FILL_FUNCT_ADDR,a1
+  jsr        (a1)
 
-    movem.l (sp)+,d0-d7/a1
-    rts
+  movem.l    (sp)+,d0-d7/a1
+  rts
 
--- libs/rasterizers/triangle.s
+++ libs/rasterizers/triangle.s
@@ -1,28 +1,22 @@
-
-
-
 ; first point d0,d1
 ; second point d6,d3
 ; third point d4,d5
 
 TRIANGLE:
-    movem.l d0-d7/a0-a1,-(sp)
-    ;RESETFILLTABLE
+  movem.l                                   d0-d6/a1,-(sp)
+    
+  POINT_TRANSFORM_AND_STORE_IN_FILLTABLE    d0,d1,0
+  POINT_TRANSFORM_AND_STORE_IN_FILLTABLE    d6,d3,4
+  bsr.w                                     ammxlinefill
+    
+  POINT_TRANSFORM_AND_STORE_IN_FILLTABLE    d4,d5,4
+  bsr.w                                     ammxlinefill
+    
+  POINT_TRANSFORM_AND_STORE_IN_FILLTABLE    d6,d3,0
+  bsr.w                                     ammxlinefill
+    
+  move.l                                    AMMX_FILL_FUNCT_ADDR,a1
+  jsr                                       (a1)
 
-    
-    POINT_TRANSFORM_AND_STORE_IN_FILLTABLE d0,d1,0
-    POINT_TRANSFORM_AND_STORE_IN_FILLTABLE d6,d3,4
-    bsr.w ammxlinefill
-    
-    POINT_TRANSFORM_AND_STORE_IN_FILLTABLE d4,d5,4
-    bsr.w ammxlinefill
-    
-    POINT_TRANSFORM_AND_STORE_IN_FILLTABLE d6,d3,0
-    bsr.w ammxlinefill
-    
-    move.l AMMX_FILL_FUNCT_ADDR,a0
-    jsr (a0)
-
-    movem.l (sp)+,d0-d7/a0-a1
-    rts
-
+  movem.l                                   (sp)+,d0-d6/a1
+  rts
