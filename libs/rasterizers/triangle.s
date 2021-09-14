bb645c19701ad47492361001f361ce49679b0f2a
Ozzyboshi <gun101@email.it>
2021-09-14 17:08:08 +0000

    TRIANGLE_NODRAW

--- libs/rasterizers/triangle.s
+++ libs/rasterizers/triangle.s
@@ -20,3 +20,19 @@ TRIANGLE:
 
   movem.l                                   (sp)+,d0-d6/a1
   rts
+
+TRIANGLE_NODRAW:
+  movem.l                                   d0-d6/a1,-(sp)
+    
+  POINT_TRANSFORM_AND_STORE_IN_FILLTABLE    d0,d1,0
+  POINT_TRANSFORM_AND_STORE_IN_FILLTABLE    d6,d3,4
+  bsr.w                                     ammxlinefill
+    
+  POINT_TRANSFORM_AND_STORE_IN_FILLTABLE    d4,d5,4
+  bsr.w                                     ammxlinefill
+    
+  POINT_TRANSFORM_AND_STORE_IN_FILLTABLE    d6,d3,0
+  bsr.w                                     ammxlinefill
+  movem.l                                   (sp)+,d0-d6/a1
+
+  rts
