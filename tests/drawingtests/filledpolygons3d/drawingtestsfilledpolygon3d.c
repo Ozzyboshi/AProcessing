57644f11f1be5167cd19c5cfb0c2c1ae00705d36
Ozzyboshi <gun101@email.it>
2021-10-05 16:47:03 +0000

    test upside down

--- tests/drawingtests/filledpolygons3d/drawingtestsfilledpolygon3d.c
+++ tests/drawingtests/filledpolygons3d/drawingtestsfilledpolygon3d.c
@@ -7,11 +7,13 @@
 #include "../../testlibs/testlib.h"
 
 unsigned char *drawing_test3d1();
+unsigned char *drawing_test3d2();
 
 
 
 struct _test TESTS[] = {
     {.testId = 1, .test_function = drawing_test3d1, .nbitplanes=2, .nrows=256, .nbyterow=40,.title = "triangle rotated filled", .verbose=0},
+    {.testId = 2, .test_function = drawing_test3d2, .nbitplanes=2, .nrows=256, .nbyterow=40,.title = "2 triangle rotated overlapped (one upside down)", .verbose=0},
   
     {.testId = 0, .test_function = NULL}
 
