2f6f2b39a90c3623546c5db17cbb6c17e15300b5
Ozzyboshi <gun101@email.it>
2021-11-04 13:28:40 +0000

    small triangle3d opt

--- libs/rasterizers/triangle3d.s
+++ libs/rasterizers/triangle3d.s
@@ -3,7 +3,7 @@
 ; third point d4,d5
   IFD                                          USE_3D
 TRIANGLE3D:
-  movem.l                                      d0-d6/a1,-(sp)
+  movem.l                                      d0-d2/a1,-(sp)
 
   POINT_TRANSFORM_AND_STORE_IN_FILLTABLE_3D    VERTEX_LIST_3D_1,VERTEX_LIST_3D_1+2,VERTEX_LIST_3D_1+4,0
   POINT_TRANSFORM_AND_STORE_IN_FILLTABLE_3D    VERTEX_LIST_3D_2,VERTEX_LIST_3D_2+2,VERTEX_LIST_3D_2+4,4
@@ -18,11 +18,11 @@ TRIANGLE3D:
   move.l                                       AMMX_FILL_FUNCT_ADDR,a1
   jsr                                          (a1)
 
-  movem.l                                      (sp)+,d0-d6/a1
+  movem.l                                      (sp)+,d0-d2/a1
   rts
 
 TRIANGLE3D_NODRAW:
-  movem.l                                      d0-d6/a1,-(sp)
+  movem.l                                      d0-d2/a1,-(sp)
 
   POINT_TRANSFORM_AND_STORE_IN_FILLTABLE_3D    VERTEX_LIST_3D_1,VERTEX_LIST_3D_1+2,VERTEX_LIST_3D_1+4,0
   POINT_TRANSFORM_AND_STORE_IN_FILLTABLE_3D    VERTEX_LIST_3D_2,VERTEX_LIST_3D_2+2,VERTEX_LIST_3D_2+4,4
@@ -34,6 +34,6 @@ TRIANGLE3D_NODRAW:
   POINT_TRANSFORM_AND_STORE_IN_FILLTABLE_3D    VERTEX_LIST_3D_2,VERTEX_LIST_3D_2+2,VERTEX_LIST_3D_2+4,0
   bsr.w                                        ammxlinefill
 
-  movem.l                                      (sp)+,d0-d6/a1
+  movem.l                                      (sp)+,d0-d2/a1
   rts
   ENDC
