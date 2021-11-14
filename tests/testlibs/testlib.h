dd28e32af261ae128c70634eac5bc39f426a380c
Ozzyboshi <gun101@email.it>
2021-11-14 00:26:55 +0000

    fsuae unit tests

--- tests/testlibs/testlib.h
+++ tests/testlibs/testlib.h
@@ -1,5 +1,9 @@
 
 #include <proto/exec.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define TEST_FUNC_IMPLEMENTATION(var,var2)\
     int main(int argc, char **argv)\
@@ -7,30 +11,61 @@
         unsigned int error;\
         unsigned int totalerrors = 0;\
         unsigned int testcounter = 0;\
-        printf("Start executing file %s\n",__FILE__);\
+        unsigned int testsperformed = 0;\
+        int c;\
+        int start=0;\
+        int end=0;\
+        char fileresult[100];\
+        int writeresult=0;\
+        while ((c = getopt (argc, argv, "s:e:o:")) != -1)\
+        {\
+            switch(c)\
+            {\
+                case 's': start = atoi(optarg); break;\
+                case 'e': end = atoi(optarg); break;\
+                case 'o': writeresult=snprintf(fileresult,99,"%s",optarg); break;\
+                default : printf("Usage: %s [-s start] [-e end] [-o resultfile]\n",argv[0]);\
+            }\
+        }\
+        printf("Start executing file %s - range %d-%d\n",__FILE__,start,end);\
     \
         while (var[testcounter].testId)\
         {\
-            if (argc == 1 || (argc==2 && atoi(argv[1]) == var[testcounter].testId) || (argc==3 && var[testcounter].testId>=atoi(argv[1]) && var[testcounter].testId<=atoi(argv[2]) ) )\
+            if (start && var[testcounter].testId < start)\
             {\
-                printf("Running test %d - %s...", var[testcounter].testId,var[testcounter].title);\
-                char filename[100];\
-                snprintf(filename, sizeof(filename), "expected/%s%d",var2, var[testcounter].testId);\
-                if (argc==2 && atoi(argv[1]) == var[testcounter].testId) var[testcounter].verbose = 3;\
-                error = make_test_patched( var[testcounter],filename,var[testcounter]);\
-                if (!error)\
-                    printf("Test succeeded\n");\
-                else\
-                    printf("Test KO\n");\
-                totalerrors += error;\
+                testcounter++;\
+                continue;\
             }\
-    \
+            if (end && var[testcounter].testId > end) break;\
+            \
+            printf("Running test %d - %s...", var[testcounter].testId,var[testcounter].title);\
+            char filename[100];\
+            snprintf(filename, sizeof(filename), "expected/%s%d",var2, var[testcounter].testId);\
+            if (argc==2 && atoi(argv[1]) == var[testcounter].testId) var[testcounter].verbose = 3;\
+            error = make_test_patched( var[testcounter],filename,var[testcounter]);\
+            if (!error)\
+                printf("Test succeeded\n");\
+            else\
+                printf("Test KO\n");\
+            totalerrors += error;\
             testcounter++;\
+            testsperformed++;\
         }\
         printf("********************************************************\n");\
-        if (totalerrors==0) printf("Result %s: All tests ok (%d)\n",__FILE__,testcounter);\
-        else printf("Result %s: ERROR:!!!!!! ***** Total errors: %d out of %d******************\n", __FILE__,totalerrors,testcounter);\
+        if (totalerrors==0) printf("Result %s: All tests ok (%d)\n",__FILE__,testsperformed);\
+        else printf("Result %s: ERROR:!!!!!! ***** Total errors: %d out of %d******************\n", __FILE__,totalerrors,testsperformed);\
         printf("********************************************************\n\n\n");\
+        if (writeresult)\
+        {\
+            FILE* fdres = fopen(fileresult,"a");\
+            if (fdres)\
+            {\
+                if (totalerrors==0) fprintf(fdres,"Result %s: All tests ok (%d)\n",__FILE__,testsperformed);\
+                else fprintf(fdres,"Result %s: ERROR:!!!!!! ***** Total errors: %d out of %d******************\n", __FILE__,totalerrors,testsperformed);\
+                fclose(fdres);\
+            }\
+            else perror("Error in writing fileresult");\
+        }\
         exit(totalerrors);\
     }
 
